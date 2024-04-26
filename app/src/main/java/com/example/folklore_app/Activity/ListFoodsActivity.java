ba074c49f737523efa332da4213d613856d37840
mac <chahedattia28@gmail.com>
2024-04-26 22:45:06 +0100

    Food List Acitivity v2

--- app/src/main/java/com/example/folklore_app/Activity/ListFoodsActivity.java
+++ app/src/main/java/com/example/folklore_app/Activity/ListFoodsActivity.java
@@ -1,7 +1,9 @@
 package com.example.folklore_app.Activity;
 
+import android.app.DownloadManager;
 import android.os.Bundle;
 import android.renderscript.ScriptGroup;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,21 +15,53 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.folklore_app.R;
 import com.example.folklore_app.databinding.ActivityListFoodsBinding;
 
-public class ListFoodsActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class ListFoodsActivity extends BaseActivity {
 ActivityListFoodsBinding binding;
 private RecyclerView.Adapter adapterListFood;
-private int category;
-
+private int categoryId;
+private String categoryName;
+private String searchText ;
+private boolean isSearch ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityListFoodsBinding.inflate(getLayoutInflater())
+        binding=ActivityListFoodsBinding.inflate(getLayoutInflater());
         EdgeToEdge.enable(this);
         setContentView(binding.getRoot());
+        getIntentExtra();
+        initList();
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void initList() {
+        DatabaseReferences myref =database.getReferences();
+        binding.progressBar.setVisibility(view.VISIBLE);
+        ArrayList<Foods> list=new ArrayList<>();
+        Query query ;
+        if(isSearch){
+            query=myref.orderByChild("Title").startAt(searchText).endAt(searchText+\'uf8ff');
+        }else{
+            query=myref.orderByChild("CategoryId").equalTo(categoryId);
+
+        }
+    }
+
+    private void getIntentExtra() {
+        categoryId=getIntent().getIntExtra("CategoryId",0);
+        categoryName=getIntent().getStringExtra("Category");
+        searchText=getIntent().getStringExtra("text");
+        isSearch=getIntent().getBooleanExtra("isSearch",false);
+        binding.titleTxt.setText(categoryName);
+        binding.backBtn.setOnClickListener(v -> finish());
+
     }
 }
