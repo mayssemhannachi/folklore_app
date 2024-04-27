f8edc665dc765c76c4ab78fecba3042aeb2058cf
mac <chahedattia28@gmail.com>
2024-04-27 01:35:22 +0100

    Dashboard BackEnd pt1

---
+++ app/src/main/java/com/example/folklore_app/Adapter/BestFoodAdapter.java
@@ -0,0 +1,50 @@
+package com.example.folklore_app.Adapter;
+
+import android.content.Context;
+import android.view.ContextThemeWrapper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.folklore_app.Domain.Foods;
+import com.example.folklore_app.R;
+
+import java.util.ArrayList;
+
+public class BestFoodAdapter extends RecyclerView.Adapter<BestFoodAdapter.viewholder> {
+    ArrayList<Foods> items;
+    Context context;
+
+    public BestFoodAdapter(ArrayList<Foods> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public BestFoodAdapter.viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context= parent.getContext();
+        View inflate= LayoutInflater.from(parent.getContext()).inflate(R.layout.);
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BestFoodAdapter.viewholder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class viewholder extends RecyclerView.ViewHolder{
+
+        public viewholder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+    }
+}
---
+++ app/src/main/java/com/example/folklore_app/Domain/Category.java
@@ -0,0 +1,37 @@
+package com.example.folklore_app.Domain;
+
+public class Category {
+    private int id;
+    private String ImagePath;
+    private String Name;
+
+    public Category() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getImagePath() {
+        return ImagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        ImagePath = imagePath;
+    }
+
+
+}
