31043a3e91a40789f1cb375f30abba3817d258bc
mayssemhannachi <h.mayssem2003@gmail.com>
2024-05-02 01:12:07 +0100

    manifest upgrade

--- app/src/main/java/com/example/folklore_app/Helper/ManagmentCart.java
+++ app/src/main/java/com/example/folklore_app/Helper/ManagmentCart.java
@@ -54,19 +54,6 @@ public class ManagmentCart {
         return fee;
     }
 
-    public void minusNumberItem(ArrayList<Foods> listItem,int position,ChangeNumberItemsListener changeNumberItemsListener){
-        if(listItem.get(position).getNumberInCart()==1){
-            listItem.remove(position);
-        }else{
-            listItem.get(position).setNumberInCart(listItem.get(position).getNumberInCart()-1);
-        }
-        tinyDB.putListObject("CartList",listItem);
-        changeNumberItemsListener.change();
-    }
-    public  void plusNumberItem(ArrayList<Foods> listItem,int position,ChangeNumberItemsListener changeNumberItemsListener){
-        listItem.get(position).setNumberInCart(listItem.get(position).getNumberInCart()+1);
-        tinyDB.putListObject("CartList",listItem);
-        changeNumberItemsListener.change();
-    }
+
 
 }
