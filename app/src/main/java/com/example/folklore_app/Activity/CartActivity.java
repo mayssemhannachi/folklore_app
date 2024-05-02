96ca2dac896fb68ad4ccdadcbafc35d853005b91
mayssemhannachi <h.mayssem2003@gmail.com>
2024-05-02 16:03:40 +0100

    final checks

--- app/src/main/java/com/example/folklore_app/Activity/CartActivity.java
+++ app/src/main/java/com/example/folklore_app/Activity/CartActivity.java
@@ -35,7 +35,7 @@ public class CartActivity extends AppCompatActivity {
         calculateCart();
         initList();
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginBtn), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.loginBtn2), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
